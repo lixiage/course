a98625e6dd80499c3ef34d2a9030cf0238d4fadf
wanghaodong <1466745663@qq.com>
2017-11-29 09:54:23 +0800

    王浩栋shop

--- laravel-v5.2.0/app/Http/Controllers/Frontend/ShopcarController.php
+++ laravel-v5.2.0/app/Http/Controllers/Frontend/ShopcarController.php
@@ -1,12 +1,12 @@
 <?php
+
 namespace App\Http\Controllers\Frontend;
 
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public function index()
-    {
-        var_dump($_SERVER['HTTP_HOST']);
-    }
+
 }
+
