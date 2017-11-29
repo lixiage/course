96fbe041aec68b98eafb7ca33d726128ae0e173b
wanghaodong <1466745663@qq.com>
2017-11-29 15:22:11 +0800

    wwhsd

--- laravel-v5.2.0/app/Http/routes-frontend.php
+++ laravel-v5.2.0/app/Http/routes-frontend.php
@@ -1,3 +1,9 @@
 <?php
 Route::get('/','Controller@index');
 Route::get('index','LoginController@index');
+
+//购物车
+Route::any('whdindex','ShopcarController@index');
+
+//订单
+Route::any('whdorder_list','OrderController@whdorder_list');
