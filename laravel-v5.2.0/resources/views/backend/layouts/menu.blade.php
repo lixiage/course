db044f6f9957c666d8e10d62acf80925087957e3
1316074551@qq.com <1316074551@qq.com>
2017-12-03 15:43:17 +0800

    6666

--- laravel-v5.2.0/resources/views/backend/layouts/menu.blade.php
+++ laravel-v5.2.0/resources/views/backend/layouts/menu.blade.php
@@ -11,99 +11,6 @@
             <li class="sub-menu">
                 <a href="javascript:;" >
                     <i class="fa fa-desktop"></i>
-<<<<<<< HEAD
-                    <span>用户管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="general.html">General</a></li>
-                    <li><a  href="buttons.html">Buttons</a></li>
-                    <li><a  href="panels.html">Panels</a></li>
-                </ul>
-            </li>
-
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class=" fa fa-bar-chart-o"></i>
-                    <span>订单管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="calendar.html">Calendar</a></li>
-                    <li><a  href="gallery.html">Gallery</a></li>
-                    <li><a  href="todo_list.html">Todo List</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-book"></i>
-                    <span>评论管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="blank.html">Blank Page</a></li>
-                    <li><a  href="login.html">Login</a></li>
-                    <li><a  href="lock_screen.html">Lock Screen</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-tasks"></i>
-                    <span>分类管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="{{ url('addDirection') }}">添加方向</a></li>
-                    <li><a  href="{{ url('addClassify') }}">添加分类</a></li>
-                    <li><a  href="{{ url('addType') }}">添加类型</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-th"></i>
-                    <span>分类列表</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="{{  url('directionList') }}">方向列表</a></li>
-                    <li><a  href="{{  url('classifyList') }}">分类列表</a></li>
-                    <li><a  href="{{  url('typeList') }}">类型列表</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-tasks"></i>
-                    <span>课程管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="{{ url('addCourse') }}">添加课程</a></li>
-                    <li><a  href="{{ url('courseList') }}">课程列表</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-tasks"></i>
-                    <span>章节管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="{{ url('addChapter') }}">添加章节</a></li>
-                    <li><a  href="{{ url('chapterList') }}">章节列表</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-tasks"></i>
-                    <span>小节管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="{{ url('addSmallMatter') }}">添加小节</a></li>
-                    <li><a  href="{{ url('smallMatterList') }}">小节列表</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-tasks"></i>
-                    <span>视频管理</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="{{ url('addVideo') }}">添加视频</a></li>
-                    <li><a  href="{{ url('videoList') }}">视频列表</a></li>
-=======
                     <span><?php echo $val['pname']?></span>
                 </a>
                 <ul class="sub">
@@ -114,7 +21,6 @@
                      <?php }?>
                     {{--<li><a  href="empower">角色管理</a></li>--}}
                     {{--<li><a  href="powerList">节点管理</a></li>--}}
->>>>>>> 17dc0c0e65fd83ac8d7b33317e321d2deaae2918
                 </ul>
             </li>
             <?php }?>
